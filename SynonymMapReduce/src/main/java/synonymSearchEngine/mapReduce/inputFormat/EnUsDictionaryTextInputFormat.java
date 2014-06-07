6f8885617383194ae9b3a1e488e4de68d4f540b9
nnhai1991 <nnhai1991@gmail.com>
2014-06-07 14:51:28 +0800

    Add Dictionary Input

--- SynonymMapReduce/src/main/java/synonymSearchEngine/mapReduce/inputFormat/EnUsDictionaryTextInputFormat.java
+++ SynonymMapReduce/src/main/java/synonymSearchEngine/mapReduce/inputFormat/EnUsDictionaryTextInputFormat.java
@@ -1,13 +1,19 @@
 package synonymSearchEngine.mapReduce.inputFormat;
 
 import java.io.IOException;
+import java.util.HashMap;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 
 public class EnUsDictionaryTextInputFormat extends FileInputFormat<LongWritable, Text>{
@@ -21,7 +27,10 @@ public class EnUsDictionaryTextInputFormat extends FileInputFormat<LongWritable,
 			TaskAttemptContext arg1) throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
 		return new RecordReader<LongWritable, Text>() {
-
+			
+			HashMap<Long, String> keyValue = new HashMap<Long, String>();
+			int pos = 0;
+			
 			@Override
 			public void close() throws IOException {
 				// TODO Auto-generated method stub
@@ -32,14 +41,16 @@ public class EnUsDictionaryTextInputFormat extends FileInputFormat<LongWritable,
 			public LongWritable getCurrentKey() throws IOException,
 					InterruptedException {
 				// TODO Auto-generated method stub
-				return null;
+				return new LongWritable(pos);
 			}
 
 			@Override
 			public Text getCurrentValue()
 					throws IOException, InterruptedException {
 				// TODO Auto-generated method stub
-				return null;
+				Text t=new Text(keyValue.get(new Long(pos)));
+				pos++;
+				return t;
 			}
 
 			@Override
@@ -49,17 +60,45 @@ public class EnUsDictionaryTextInputFormat extends FileInputFormat<LongWritable,
 			}
 
 			@Override
-			public void initialize(InputSplit arg0, TaskAttemptContext arg1)
-					throws IOException, InterruptedException {
-				// TODO Auto-generated method stub
-				
+			public void initialize(InputSplit genericSplit,
+					TaskAttemptContext context) throws IOException,
+					InterruptedException {
+				// This InputSplit is a FileInputSplit
+				FileSplit split = (FileSplit) genericSplit;
+				// Retrieve file containing Split "S"
+				final Path file = split.getPath();
+				// Retrieve configuration, and Max allowed
+				// bytes for a single record
+				Configuration job = context.getConfiguration();
+
+				FileSystem fs = file.getFileSystem(job);
+				FSDataInputStream fileIn = fs.open(split.getPath());
+				StringBuffer inputLine = new StringBuffer();
+				String tmp;
+				while ((tmp = fileIn.readLine()) != null) {
+					inputLine.append(tmp);
+			//		System.out.println(tmp);
+				}
+				String fileInString=inputLine.toString();
+				String s[]=fileInString.split("\n");
+				long index =0;
+				for(int i=0;i<s.length;i++)
+				{
+					if (s[i].startsWith("(")){
+						keyValue.put(new Long(index), s[i]);
+						index++;
+					}
+				}				
 			}
 
 			@Override
 			public boolean nextKeyValue() throws IOException,
 					InterruptedException {
 				// TODO Auto-generated method stub
-				return false;
+				if (keyValue.get(new Long(pos)) == null)
+					return false;
+				else
+					return true;
 			}
 		};
 	}
